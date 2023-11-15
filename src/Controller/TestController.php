a34306f1f4ca47f3e16938bda27d4aee9097a7ff
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-11-15 14:44:28 +0100

    Fin TP3

---
+++ src/Controller/TestController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Faker\Factory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test/twig', name: 'app_test_twig')]
+    public function index(): Response
+    {
+        $faker = Factory::create('fr_FR');
+
+        $users = [];
+        for($i = 0; $i < 9; $i++){
+            $user = [
+                'name' => $faker->name(),
+                'email' => $faker->email(),
+                'age' => $faker->randomNumber(2, false),
+                'address' => [
+                   'street' => $faker->streetName(),
+                   'code postal' => $faker->postcode(),
+                   'city' => $faker->city(),
+                   'country' => $faker->country(),
+                ],
+                'picture' => $faker->imageUrl(360, 360, 'animals', true, 'dogs', true, 'jpg'),
+                'cover' => $faker->imageUrl(360, 360, 'users', true, 'user', true, 'jpg'),
+                'createAt' => $faker->date()
+
+            ];
+            $users[$i] = $user;
+        }
+        return $this->render('test/index.html.twig',[
+            'title' => 'page Accueil',
+            'users' => $users,
+        ]);
+    }
+}
